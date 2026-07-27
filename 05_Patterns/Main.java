3dd08536b561917c9f7a1a862ba3a556f891681b
GENIUS-69 <galaxygaming0511@gmail.com>
2026-07-27 14:28:24 +0530

    Pattern and Recursion

---
+++ 05_Patterns/Main.java
@@ -0,0 +1,70 @@
+public class Main {
+  public static void main(String[] args) {
+    // pattern1(5);
+    // pattern2(5);
+    // pattern3(5);
+    // pattern4(5);
+    // pattern5(5);
+    pattern6(5);
+  }
+
+  static void pattern1(int n) {
+    for (int i = 1; i <= n; i++) {
+      for (int j = 1; j <= n; j++) {
+        System.out.print("*");
+      }
+      System.out.println();
+    }
+  }
+
+  static void pattern2(int n) {
+    for (int i = 1; i <= n; i++) {
+      for (int j = 1; j <= i; j++) {
+        System.out.print("*");
+      }
+      System.out.println();
+    }
+  }
+
+  static void pattern3(int n) {
+    for (int i = 1; i <= n; i++) {
+      for (int j = n; j >= i; j--) {
+        System.out.print("*");
+      }
+      System.out.println();
+    }
+  }
+
+  static void pattern4(int n) {
+    for (int i = 1; i <= n; i++) {
+      for (int j = 1; j <= i; j++) {
+        System.out.print(j);
+      }
+      System.out.println();
+    }
+  }
+
+  static void pattern5(int n) {
+    for (int i = 1; i < 2 * n; i++) {
+      int c = i > n ? 2 * n - i : i;
+      for (int j = 0; j < c; j++) {
+        System.out.print("*");
+      }
+      System.out.println();
+    }
+  }
+
+  static void pattern6(int n) {
+    for (int i = 1; i <= n; i++) {
+      for (int j = 1; j <= n; j++) {
+        if (j < i) {
+          System.out.print(" ");
+        } else {
+          System.out.print("*");
+        }
+      }
+
+      System.out.println();
+    }
+  }
+}
---
+++ 06_Recursion/BinarySearch.java
@@ -0,0 +1,24 @@
+
+public class BinarySearch {
+  public static void main(String[] args) {
+    int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+    boolean result = BS(arr, 6, 0, arr.length - 1);
+    System.out.println(result);
+  }
+
+  static boolean BS(int[] arr, int key, int start, int end) {
+    if (start > end) {
+      return false; // Base case: search space is empty
+    }
+
+    int mid = start + (end - start) / 2;
+
+    if (arr[mid] == key) {
+      return true;
+    } else if (arr[mid] > key) {
+      return BS(arr, key, start, mid - 1); // Return recursive call
+    } else {
+      return BS(arr, key, mid + 1, end); // Return recursive call
+    }
+  }
+}
